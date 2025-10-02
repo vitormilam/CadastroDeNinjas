9fbd4dcd682c9e3f61bd4a34220eeb606480ce68
vitormilam <vijmilam@gmail.com>
2025-10-02 08:29:53 -0300

    Criado a Controller

---
+++ src/main/java/dev/java10X/CadastroDeNinjas/Controller.java
@@ -0,0 +1,30 @@
+package dev.java10X.CadastroDeNinjas;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    /*
+    * Principais Annotations do Spring
+    * @GetMapping -> Pegar Informações
+    * @PostMapping -> Mandar informações
+    * @PutMapping -> Alterar Informações
+    * @PatchMapping -> Altera Informações
+    * @DeleteMapping -> Deletar as Informações
+    *
+    * */
+
+    @GetMapping("/")
+    public String home(){
+        return "Essa é a home do site";
+    }
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return  "Boas-vindas";
+    }
+
+}
