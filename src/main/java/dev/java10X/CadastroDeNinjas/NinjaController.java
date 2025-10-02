6b58fe4c2be089519a6f352e9c3487f69ea6eaf2
vitormilam <vijmilam@gmail.com>
2025-10-02 10:36:12 -0300

    Adicionei dependência JPA e criei Entidade para banco de dados

--- src/main/java/dev/java10X/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/java10X/CadastroDeNinjas/NinjaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
 
     /*
     * Principais Annotations do Spring
---
+++ src/main/java/dev/java10X/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,54 @@
+package dev.java10X.CadastroDeNinjas;
+import jakarta.persistence.*;
+
+
+@Entity // Entity ele transforma uma classe em uma entidade do BD
+@Table(name = "tb_cadastro_de_ninjas") // Define o nome da tabela
+public class NinjaModel {
+
+    // Variaveis
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    // Construtores
+    NinjaModel(){
+
+    }
+
+    NinjaModel(String nome, String email, int idade){
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    // Getters & Setters
+    public String getNome(){
+        return nome;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+
+}
