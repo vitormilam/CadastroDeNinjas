0ed360c1823e4e043e4ef08c2c2bf487fee68df3
vitormilam <vijmilam@gmail.com>
2025-10-06 14:21:03 -0300

    fix: adicionando repositorys que extendes os seus respectivos models

---
+++ src/main/java/dev/java10X/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,9 @@
+package dev.java10X.CadastroDeNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+
+
+
+}
