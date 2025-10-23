f95a8d0cbdc7645161d928d578ccc2c8a20be2a0
vitormilam <vijmilam@gmail.com>
2025-10-23 10:00:18 -0300

    fix: adicionei as missoes que estavam faltando na tb missoes no banco de dados

---
+++ src/main/resources/db/migrations/V3__Add_novas_missoes.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_missoes (nome_missao, dificuldade_missao)
+VALUES
+  ('Missão de Rank D - Patrulhar a vila', 'D'),
+  ('Missão de Rank C - Escoltar mercadores', 'C'),
+  ('Missão de Rank B - Proteger o feudo', 'B'),
+  ('Missão de Rank A - Capturar criminoso', 'A'),
+  ('Missão de Rank S - Proteger o Hokage', 'S');
